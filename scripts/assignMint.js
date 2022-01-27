a2f2b89f8d554f55659bc7672578350dcf7f773e
Akram <akram.development50@gmail.com>
2022-01-26 23:13:48 -0600

    Updated styling of code make it a little easier to read

--- scripts/assignMint.js
+++ scripts/assignMint.js
@@ -1,47 +1,48 @@
+const {
+    updateMint,
+    assignTokens,
+    filterTX,
+    getAssigned
+} = require('../db/db');
+const {
+    mintNFT,
+    sleep
+} = require("../imxMethods/mint")
 
 
-const {updateMint,assignTokens,filterTX,getAssigned } = require('../db/db');
-const {mintNFT,sleep} = require("../imxMethods/mint")
+const assignMint = (element) => {
 
-
-const assignMint = (element)=> { 
-
-    return new Promise( async (resolve,reject)=> { 
+    return new Promise(async (resolve, reject) => {
         let err = false
 
-        for (i=0; i<element.quantity;i++)
-        {   
-            await sleep(parseInt(`${i}000`))  
-            const assignedTokens = await assignTokens(1,element.from.toLowerCase(),element.hash)
+        for (i = 0; i < element.quantity; i++) {
+            await sleep(parseInt(`${i}000`))
+            const assignedTokens = await assignTokens(1, element.from.toLowerCase(), element.hash)
 
-            try 
-            { 
-                const mintResult = await mintNFT(element.from.toLowerCase(),assignedTokens[0].id)
+            try {
+                const mintResult = await mintNFT(element.from.toLowerCase(), assignedTokens[0].id)
                 console.log(`${mintResult.token} has been minted to the address of ${element.from}`)
-               
+
+            } catch (err) {
+                err = true
+                console.log(err)
+                console.log(element.from.toLowerCase(), "<------ There was an issue with token ID", assignedTokens[0].id)
             }
 
-            catch (err)
-             {
-                 err = true
-                  console.log(err)
-                  console.log(element.from.toLowerCase(),"<------ There was an issue with token ID", assignedTokens[0].id)
-             }
+        }
+
+        if (err == true) {
+            updateMint(element.hash, element.from, "pending_retry")
 
         }
 
-        if (err == true)
-        { 
-            updateMint(element.hash,element.from,"pending_retry")
-
+        if (err == false) {
+            updateMint(element.hash, element.from, "success")
         }
 
-        if (err == false)
-        { 
-            updateMint(element.hash,element.from,"success")
-        }
-
-        resolve({retry:"done"})
+        resolve({
+            retry: "done"
+        })
     })
 }
 
@@ -49,19 +50,19 @@ const assignMint = (element)=> {
 
 
 
-const start = async ()=> { 
+const start = async () => {
     console.log("Starting start script")
 
     while (true) {
         await sleep(60000)
         filterTX("pending_mint")
-        .then( async (data)=> { 
-            data.forEach( async (element,index) => {
-                await sleep(parseInt(`${index}000` + 1000))
-                await assignMint(element)
-        
-            });
-        })
+            .then(async (data) => {
+                data.forEach(async (element, index) => {
+                    await sleep(parseInt(`${index}000` + 1000))
+                    await assignMint(element)
+
+                });
+            })
     }
 
 }
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const {eligableMint} = require("./imxMethods/checkTransactions")
+const {
+    eligableMint
+} = require("./imxMethods/checkTransactions")
 const cors = require("cors")
-const {mintNFT,sleep} = require("./imxMethods/mint")
-const {assignTokens,getallTx,getSaleInfo, whitelisted,getNumberMinted, getLeft,updateMint} = require("./db/db");
-const { gettxStatus } = require('./db/db');
+const {
+    mintNFT,
+    sleep
+} = require("./imxMethods/mint")
+const {
+    assignTokens,
+    getallTx,
+    getSaleInfo,
+    whitelisted,
+    getNumberMinted,
+    getLeft,
+    updateMint
+} = require("./db/db");
+const {
+    gettxStatus
+} = require('./db/db');
 const helmet = require("helmet")
 require('dotenv').config()
 
@@ -12,70 +27,64 @@ app.use(express.json())
 app.use(cors())
 app.use(helmet())
 
-app.get("/api/v1/sale/:address",async (req,res)=> { 
-    
+app.get("/api/v1/sale/:address", async (req, res) => {
+
     const SALE_INFO = await getSaleInfo()
 
     const REMAINING_NFT = await getLeft()
-    
+
     console.log(SALE_INFO)
     console.log(REMAINING_NFT)
-    if (REMAINING_NFT >= process.env.COLLECTION_SIZE)
-     {
+    if (REMAINING_NFT >= process.env.COLLECTION_SIZE) {
 
 
-        res.status(401).send({sale:"SOLD OUT"})
+        res.status(401).send({
+            sale: "SOLD OUT"
+        })
 
-     }
+    } else if (SALE_INFO[0].presale == true) {
+        try {
+            await whitelisted(req.params.address)
 
-     else if(SALE_INFO[0].presale == true)
-     { 
-        try 
-        { 
-        await whitelisted(req.params.address)
+            if (SALE_INFO[0].limit > 0) {
+                const NUMBER_MINTED = await getNumberMinted(req.params.address)
 
-        if (SALE_INFO[0].limit > 0)
-        { 
-        const NUMBER_MINTED = await getNumberMinted(req.params.address)
+                const ELIGABLE_AMOUNT = SALE_INFO[0].limit - NUMBER_MINTED
 
-        const ELIGABLE_AMOUNT = SALE_INFO[0].limit - NUMBER_MINTED
-
-        if (ELIGABLE_AMOUNT <= 0)
-        { 
-            res.status(401).send({"sale":"Sale Limit Exceeded"})
-        }
-
-        else 
-        { 
-            res.send({"sale":ELIGABLE_AMOUNT})
-        }
+                if (ELIGABLE_AMOUNT <= 0) {
+                    res.status(401).send({
+                        "sale": "Sale Limit Exceeded"
+                    })
+                } else {
+                    res.send({
+                        "sale": ELIGABLE_AMOUNT
+                    })
+                }
+            } else {
+                res.send({
+                    "sale": "Sale does not have a limit"
+                })
             }
-        else 
-        { 
-        res.send({"sale":"Sale does not have a limit"})
+            // res.send({success:"Authorized.."})
+        } catch {
+
+            res.status(401).send({
+                "error": "Unauthorized"
+            })
         }
-        // res.send({success:"Authorized.."})
+    } else if (SALE_INFO[0].public == true) {
+
+        res.send({
+            sale: "Public sale is live!"
+        })
+    } else if (SALE_INFO[0].public == false && SALE_INFO[0].presale == false) {
+
+        res.status(401).send({
+            sale: "Sale has ended"
+        })
     }
 
-        catch{
-
-        res.status(401).send({"error":"Unauthorized"})
-        }
-     }
-
-
-     else if(SALE_INFO[0].public == true)
-     { 
-        
-    res.send({sale:"Public sale is live!"})
-     }
-     else if (SALE_INFO[0].public == false && SALE_INFO[0].presale == false)
-     { 
-    
-        res.status(401).send({sale:"Sale has ended"})
-    }
-    
-    })
+})
 
 
 
@@ -83,86 +92,79 @@ app.get("/api/v1/sale/:address",async (req,res)=> {
 
 
 
-app.get("/api/v1/tx/:address", async (req,res)=> { 
+app.get("/api/v1/tx/:address", async (req, res) => {
 
     const result = await getallTx(req.params.address)
-  res.send(result)
+    res.send(result)
 
 })
- 
 
 
 
-app.get("/api/v1/count", async (req,res)=> { 
+
+app.get("/api/v1/count", async (req, res) => {
 
     const remaining = await getLeft()
-    res.send({count:remaining})
+    res.send({
+        count: remaining
+    })
 
 })
- 
 
 
-app.post("/api/v1/mint",eligableMint, async (req,res)=> { 
-    
-   console.log("Initiating mints for ",req.body.address)
-   const hashStatus =  await gettxStatus(req.body.hash)
-   let errs = false
 
-   try 
-   { 
-    
+app.post("/api/v1/mint", eligableMint, async (req, res) => {
 
-    if (hashStatus[0].status == "mintQueue")
-    { 
-        for (i=0; i<req.body.quantity;i++)
-        {   
-            await sleep(1000 + parseInt(`${i}000`))  
+    console.log("Initiating mints for ", req.body.address)
+    const hashStatus = await gettxStatus(req.body.hash)
+    let errs = false
 
-            try 
-            { 
-                const assignedTokens = await assignTokens(1,req.body.address.toLowerCase(),req.body.hash)
+    try {
 
-                const mintResult = await mintNFT(req.body.address.toLowerCase(),assignedTokens[0].id)
-                console.log(`${mintResult.token} has been minted to the address of ${req.body.address}`)
-               
-            }
 
-            catch (err)
-            {
+        if (hashStatus[0].status == "mintQueue") {
+            for (i = 0; i < req.body.quantity; i++) {
+                await sleep(1000 + parseInt(`${i}000`))
 
-            console.log(err)
-            errs = true
-            updateMint(req.body.hash,req.body.address,"pending_retry")
-            console.log(req.body.address.toLowerCase(),"<------ There was an issue with token ID", assignedTokens[0].id)
+                try {
+                    const assignedTokens = await assignTokens(1, req.body.address.toLowerCase(), req.body.hash)
+
+                    const mintResult = await mintNFT(req.body.address.toLowerCase(), assignedTokens[0].id)
+                    console.log(`${mintResult.token} has been minted to the address of ${req.body.address}`)
+
+                } catch (err) {
+
+                    console.log(err)
+                    errs = true
+                    updateMint(req.body.hash, req.body.address, "pending_retry")
+                    console.log(req.body.address.toLowerCase(), "<------ There was an issue with token ID", assignedTokens[0].id)
+
+                }
 
             }
 
+            if (errs) {
+                res.send({
+                    status: "Tokens being processed!"
+                })
+            } else {
+                updateMint(req.body.hash, req.body.address, "success")
+                res.send({
+                    "success": "Tokens have been proccessed!"
+                })
+            }
         }
 
-        if (errs)
-        { 
-            res.send({status:"Tokens being processed!"})
-        }
-
-        else 
-        { 
-            updateMint(req.body.hash,req.body.address,"success")
-            res.send({"success":"Tokens have been proccessed!"})
-        }
+    } catch (err) {
+        res.send({
+            err: "There was an error"
+        })
     }
-
-}
-
-
-catch (err)
-{ 
-res.send({err:"There was an error"})
-}
 
 })
 
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT,err=> { 
+app.listen(PORT, err => {
     console.log(`MINT SERVICE IS RUNNING ON PORT: ${PORT}`)
-})  
+})
